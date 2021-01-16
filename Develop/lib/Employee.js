824e885de55c06aa3cb18377379bac83cf632738
Babakbigdeli <babakbigdeli@gmail.com>
2021-01-16 15:19:22 +0000

    Employee class defined

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,1 +1,23 @@
-// TODO: Write code to define and export the Employee class
+// Defines the Employee class and methods and exporting it
+
+class Employee {
+    constructor(name, id, email){
+        this.name = name
+        this.id = id
+        this.email = email
+    }
+    getName(){
+        return this.name
+    }
+    getId(){
+        return this.id
+    }
+    getEmail(){
+        return this.email
+    }
+    getRole(){
+        return "Employee"
+    }
+}
+
+module.exports = Employee
